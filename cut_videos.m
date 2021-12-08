2f1cbb0b0be081fbdad55c42424b0ed44ee3ebed
Siyang Song <songsiyang0505@hotmail.com>
2021-12-08 18:26:45 +0000

    Update cut_videos.m

--- cut_videos.m
+++ cut_videos.m
@@ -4,7 +4,7 @@
 % Email: siyang.song@nottingham.ac.uk
 function [raw_data,num_keep_frame,raw_num_keep_frame] = cut_videos(raw_data,fre_resolution)
 
-len = size(raw_data,1);
+len = size(raw_data,2);
 raw_num_keep_frame = floor(len/fre_resolution);
 num_keep_frame = raw_num_keep_frame*fre_resolution;
 num_delete = len - num_keep_frame;
@@ -12,7 +12,7 @@ num_delete = len - num_keep_frame;
 num_delete_start = ceil(num_delete/2);
 num_delete_end = floor(num_delete/2);
 
-raw_data([1:num_delete_start,end-num_delete_end+1:end],:)=[];
+raw_data(:,[1:num_delete_start,end-num_delete_end+1:end])=[];
 
 end
 
